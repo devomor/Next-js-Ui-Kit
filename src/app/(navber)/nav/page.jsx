664b00eee2ec009d71a1693f349f129775eeda65
softdevomar <omorfarukqqq@gmail.com>
2024-07-01 09:13:32 -0700

    new navber add

--- src/app/(navber)/nav/page.jsx
+++ src/app/(navber)/nav/page.jsx
@@ -31,6 +31,22 @@ const Page = () => {
           <button className='bg-[#3A4F39] px-10 h-9 text-fuchsia-50 font-sans rounded-sm' >Buy</button>
        </div>
      </nav>
+       <h1>Navbar-3</h1>
+       <nav className="  bg-gray-100 h-14  content-center pr-10" >
+       <div className="flex justify-between items-center ">
+          <div className="flex pl-10 content-center">
+          <Image height={50} width={50} src={instragram} />
+          <Image height={50} width={50} src={x} />
+          <Image height={50} width={50} src={be} />
+          </div>
+          <ul className='flex gap-5 uppercas'>
+            <li className='px-3 p-1 rounded-sm bg-fuchsia-100 hover:bg-lime-600 px-3 p-1 rounded-sm'>Home</li>
+            <li className='px-3 p-1 rounded-sm bg-fuchsia-100 hover:bg-lime-600 px-3 p-1 rounded-sm'>About</li>
+            <li className='px-2 p-1 rounded-sm bg-fuchsia-100 hover:bg-lime-600 px-3 p-1 rounded-sm'>Contact</li>
+          </ul>
+          <button className='bg-[#3A4F39] px-10 h-9 text-fuchsia-50 font-sans rounded-sm' >Buy</button>
+       </div>
+     </nav>
     </div>
   );
 }
--- src/components/gride_component.jsx
+++ src/components/gride_component.jsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import logo from '../../public/Logo.png';
 const GrideComponent = ({ key,title,description}) => {
     return (
-        <div key={key} className=' p-2  w-56 rounded-md shadow-2xl bg-slate-100'>
+        <div key={key} className=' p-2  w-56 rounded-md shadow-2xl bg-slate-100 '>
             <Image height={200}  width='full' src={logo}/>
             <div className='h-2'></div>
             <h1 className=' font-sans text-xl '>{title}</h1>
             <p className=' font-sans line-clamp-3 overflow-hidden text-ellipsis'>{description}</p>
               <div className='h-2'></div>
             <center>
-                <button className=' bg-green-500 px-7 p-1 rounded-md shadow-sm text-fuchsia-50'>Details</button>
+                <button className=' bg-green-500 px-7 p-1 rounded-md shadow-sm text-fuchsia-50 hover:bg-red-500'>Details</button>
             </center>
         </div>
     );
