e50498db4d0fc49f78ee8f7cab5e3e1843381156
softdevomar <omorfarukqqq@gmail.com>
2024-07-01 08:28:25 -0700

    gride style add

--- src/app/(gride)/gride/page.jsx
+++ src/app/(gride)/gride/page.jsx
@@ -1,9 +1,33 @@
-
+"use client"
+import GrideComponent from "@/components/gride_component";
+import { useEffect, useState } from 'react';
 const GridePage = () => {
+ const [grideItems, setGrideItems] = useState([]);
+      useEffect(() => {
+    const fetchGrideItems = async () => {
+      try {
+        const {grideData} = await import('../../../models/gride_model.js');
+        setGrideItems(grideData);
+      } catch (error) {
+        console.error('Error fetching navbar items:', error);
+      }
+    };
+
+    fetchGrideItems();
+  }, []);
+
     return (
-        <div>
-            
-        </div>
+       <div className="flex flex-wrap gap-4 justify-center">
+         {grideItems.map(items=>(
+            <GrideComponent 
+            key={items.id} 
+            title={items.title} 
+            description={items.description} 
+            />
+         ))}
+       </div>
+
+      
     );
 }
 
--- src/models/gride_model.js
+++ src/models/gride_model.js
@@ -1,3 +1,4 @@
+
 class GrideModels{
     constructor(id, title,description) {
     this.id = id;
@@ -11,6 +12,11 @@ const grideData = [
   new GrideModels(3,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley'),
   new GrideModels(4,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley'),
   new GrideModels(5,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley'),
+  new GrideModels(6,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1600s, when an unknown printer took a galley'),
+  new GrideModels(7,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1700s, when an unknown printer took a galley'),
+  new GrideModels(8,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1800s, when an unknown printer took a galley'),
+  new GrideModels(9,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1900s, when an unknown printer took a galley'),
+  new GrideModels(10,'This is a title','Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1900s, when an unknown printer took a galley'),
 ];
 module.exports = {
   grideData
